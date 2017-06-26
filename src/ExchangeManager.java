ebe44aaf2c64dfbfa55fb6b8b343bc249e04b4aa
gorylpe <piotr.przestrzelski1@gmail.com>
2017-06-27 00:26:33 +0200

    exchange manager interface

---
+++ src/ExchangeManager.java
@@ -0,0 +1,48 @@
+
+
+public interface ExchangeManager {
+    boolean initializeDatabase();
+    boolean update();
+    void attachObserver(CurrencyObserver observer);
+    void notifyAllObservers();
+
+    class AutoUpdateThread extends Thread{
+
+        private final ExchangeManager manager;
+        private final long interval;
+        private boolean run;
+
+        public AutoUpdateThread(ExchangeManager manager, long interval){
+            this.manager = manager;
+            this.interval = interval;
+            this.run = true;
+        }
+
+        @Override
+        public void run(){
+            System.out.println("Starting autoupdate thread");
+
+            while(run){
+                if(!manager.update()){
+                    System.out.println("updating failed");
+                }
+                try {
+                    Thread.sleep(interval);
+                } catch(InterruptedException e){
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        public void end(){
+            System.out.println("Ending autoupdate thread");
+
+            try{
+                run = false;
+                this.join();
+            } catch(InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+    }
+}
