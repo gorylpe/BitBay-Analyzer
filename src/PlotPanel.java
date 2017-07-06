1bc9a89e9902a3a916458e64c8ee08fbcf2f2fe8
gorylpe <piotr.przestrzelski1@gmail.com>
2017-07-06 16:18:59 +0200

    simple drawing

--- src/PlotPanel.java
+++ src/PlotPanel.java
@@ -1,23 +1,25 @@
+import model.BitBayCurrencyData;
 import model.BitBayTradeJSON;
 
 import javax.swing.*;
 import java.awt.*;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.Date;
 
 public class PlotPanel extends JPanel {
 
-    ArrayList<BitBayTradeJSON> data;
+    ArrayList<BitBayCurrencyData> data;
 
     public PlotPanel(){
         super();
         setPreferredSize(new Dimension(1280, 720));
     }
 
-    public void setData(ArrayList<BitBayTradeJSON> data) {
+    public void setData(ArrayList<BitBayCurrencyData> data) {
         this.data = data;
     }
 
@@ -30,15 +32,13 @@ public class PlotPanel extends JPanel {
         g2d.setColor(Color.WHITE);
         g2d.drawRect(0, 0, getWidth(), getHeight());
         if(data != null && data.size() > 0){
-            long x0 = data.get(0).getUnixTimestamp();
-            long x1 = data.get(data.size() - 1).getUnixTimestamp();
-            double y0 = data.stream().min(Comparator.comparing(BitBayTradeJSON::getPrice)).get().getPrice();
-            double y1 = data.stream().max(Comparator.comparing(BitBayTradeJSON::getPrice)).get().getPrice();
+            double y0 = data.stream().min(Comparator.comparing(BitBayCurrencyData::getAverage)).get().getAverage() * 0.9;
+            double y1 = data.stream().max(Comparator.comparing(BitBayCurrencyData::getAverage)).get().getAverage() * 1.1;
 
-            double width = (double)(x1 - x0);
+            double width = data.size();
             double height = y1 - y0;
 
-            System.out.println(width + " " + height);
+            System.out.println(data.size() + " " + height);
 
             g2d.setColor(Color.BLACK);
             g2d.setStroke(new BasicStroke(2));
@@ -46,23 +46,27 @@ public class PlotPanel extends JPanel {
             g2d.drawString(Double.toString(y1), 10, 10);
             g2d.drawString(Double.toString(y0), 10, getHeight() - 30);
 
-            DateFormat formatter = new SimpleDateFormat("HH:mm:ss");
+            g2d.drawString(data.get(0).getPeriodStart().toString(), 10, getHeight() - 10);
+            g2d.drawString(data.get(data.size() - 1).getPeriodStart().toString(), getWidth() - 100, getHeight() - 10);
 
-            g2d.drawString(formatter.format(new Date(x0 * 1000)), 10, getHeight() - 10);
-            g2d.drawString(formatter.format(new Date(x1 * 1000)), getWidth() - 50, getHeight() - 10);
+            int lastx = 0;
+            int lastAverageY = (int)((y1 - data.get(0).getAverage()) * getHeight() / height);
 
-            int lastx = (int)((data.get(0).getUnixTimestamp() - x0) * getWidth() / width);
-            int lasty = (int)((y1 - data.get(0).getPrice()) * getHeight() / height);
+            final double xscale = getWidth() / width;
+            final double yscale = getHeight() / height;
+
             for(int i = 1; i < data.size(); ++i){
-                BitBayTradeJSON trade = data.get(i);
-                int x = (int)((trade.getUnixTimestamp() - x0) * getWidth() / width);
-                int y = (int)((y1 - trade.getPrice()) * getHeight() / height);
-                System.out.println(x + " " + y);
+                BitBayCurrencyData currencyData = data.get(i);
+                int x = (int)(i * xscale);
+                int averageY = (int)((y1 - currencyData.getAverage()) * yscale);
 
-                g2d.drawLine(lastx, lasty, x, y);
+                g2d.setColor(Color.BLACK);
+                g2d.drawLine(lastx, lastAverageY, x, averageY);
+                g2d.setColor(Color.GRAY);
+                g2d.drawLine(x, (int)((y1 - currencyData.getMinimum()) * yscale), x, (int)((y1 - currencyData.getMaximum()) * yscale));
 
                 lastx = x;
-                lasty = y;
+                lastAverageY = averageY;
             }
         }
     }
