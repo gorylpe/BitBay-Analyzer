bf7d1811b53adcc7882c2c9eb7959e68ab952d8f
gorylpe <piotr.przestrzelski1@gmail.com>
2017-07-06 16:13:53 +0200

    default dataset plot

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,17 +1,12 @@
-import com.google.gson.Gson;
-import model.TradeJSON;
+import model.BitBayCurrencyData;
 
 import javax.swing.*;
 import javax.swing.border.Border;
-import java.io.IOException;
-import java.net.URL;
-import java.util.*;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.function.Function;
-import java.util.function.Predicate;
-import java.util.stream.Collectors;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 
-public class MainFrame extends JFrame {
+public class MainFrame extends JFrame implements BitBayCurrencyObserver {
 
     PlotPanel plotPanel;
 
@@ -26,43 +21,21 @@ public class MainFrame extends JFrame {
 
         plotPanel = new PlotPanel();
 
-        plotPanel.setData(getData());
-        plotPanel.repaint();
-
         add(plotPanel);
         setVisible(true);
         pack();
     }
 
-    public static <T> Predicate<T> distinctByKey(Function<? super T,Object> keyExtractor) {
-        Map<Object,Boolean> seen = new ConcurrentHashMap<>();
-        return t -> seen.putIfAbsent(keyExtractor.apply(t), Boolean.TRUE) == null;
-    }
+    @Override
+    public void update(BitBayManager manager) {
+        System.out.println("MainFrame updating");
+        ArrayList<BitBayCurrencyData> currencyDataArray = manager.getCurrencyDataFromPeriod(
+                BitBayManager.TradeType.ETHPLN,
+                LocalDateTime.of(2017, 4, 1, 0, 0, 0),
+                LocalDateTime.of(2017, 6, 27, 0, 0, 0),
+                ExchangeManager.CurrencyDataPeriodType.DAILY);
 
-    public ArrayList<TradeJSON> getData(){
-        ArrayList<TradeJSON> trades = new ArrayList<>();
-
-        try {
-            URL url = new URL("https://bitbay.net/API/Public/ETHPLN/trades.json?sort=desc");
-            String jsonString = new Scanner(url.openStream()).useDelimiter("\\A").next();
-            Gson gson = new Gson();
-            TradeJSON[] tmp = gson.fromJson(jsonString, TradeJSON[].class);
-            trades.addAll(Arrays.asList(tmp));
-
-            for(int i = 1; i < 20; ++i){
-                Long lastTid = tmp[tmp.length - 1].getTid();
-                url = new URL("https://bitbay.net/API/Public/ETHPLN/trades.json?sort=asc&"+"since="+(lastTid - 50 * i));
-                jsonString = new Scanner(url.openStream()).useDelimiter("\\A").next();
-                tmp = gson.fromJson(jsonString, TradeJSON[].class);
-                trades.addAll(Arrays.asList(tmp));
-                trades = trades.stream().filter(distinctByKey(TradeJSON::getTid)).collect(Collectors.toCollection(ArrayList::new));
-            }
-        } catch(IOException e){
-            e.printStackTrace();
-        }
-
-        trades.sort(Comparator.comparing(TradeJSON::getDate));
-
-        return trades;
+        plotPanel.setData(currencyDataArray);
+        plotPanel.repaint();
     }
 }
