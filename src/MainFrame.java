446e58d39820e7d8916b6e253473215155563268
gorylpe <piotr.przestrzelski1@gmail.com>
2017-07-20 10:37:15 +0200

    moved currency observer

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,41 +1,27 @@
 import model.BitBayCurrencyData;
 
 import javax.swing.*;
-import javax.swing.border.Border;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
+import java.awt.*;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
-public class MainFrame extends JFrame implements BitBayCurrencyObserver {
-
-    PlotPanel plotPanel;
+public class MainFrame extends JFrame {
 
     public MainFrame(){
         super("BitBay Analyzer");
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
-        JPanel contentPanel = new JPanel();
-        Border padding = BorderFactory.createEmptyBorder(10, 10, 10, 10);
-        contentPanel.setBorder(padding);
-        setContentPane(contentPanel);
+        try {
+            BitBayWindow bitBayWindow = new BitBayWindow();
+            setContentPane(bitBayWindow.getPanelMain());
+        } catch (SQLException e){
+            e.printStackTrace();
+            JOptionPane.showMessageDialog(this, "Error creating bitbaywindow");
+        }
 
-        plotPanel = new PlotPanel();
-
-        add(plotPanel);
         setVisible(true);
         pack();
-    }
-
-    @Override
-    public void update(BitBayManager manager) {
-        System.out.println("MainFrame updating");
-        ArrayList<BitBayCurrencyData> currencyDataArray = manager.getCurrencyDataFromPeriod(
-                BitBayManager.TradeType.ETHPLN,
-                LocalDateTime.of(2017, 4, 1, 0, 0, 0),
-                LocalDateTime.of(2017, 6, 27, 0, 0, 0),
-                ExchangeManager.CurrencyDataPeriodType.DAILY);
-
-        plotPanel.setData(currencyDataArray);
-        plotPanel.repaint();
     }
 }
