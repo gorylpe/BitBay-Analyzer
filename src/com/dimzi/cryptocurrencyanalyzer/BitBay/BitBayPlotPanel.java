98ac4ca4afe5e7557fb50ac5d5c6e13bb6262ba1
gorylpe <piotr.przestrzelski1@gmail.com>
2017-08-02 16:20:12 +0200

    added volume and horizontal line drawings

--- src/com/dimzi/cryptocurrencyanalyzer/BitBay/BitBayPlotPanel.java
+++ src/com/dimzi/cryptocurrencyanalyzer/BitBay/BitBayPlotPanel.java
@@ -16,65 +16,72 @@ public class BitBayPlotPanel extends JPanel implements MouseListener, MouseMotio
     private double valueRangeSize;
     private double valueMin;
     private double valueMax;
+    private double volumeMin;
+    private double volumeMax;
+    private double volumeRangeSize;
 
     private boolean isMouseOver;
     private int mouseX;
     private int mouseY;
 
-    public enum PlotType{
-        AVERAGES("Averages"),
-        CLOSINGS("Closings"),
-        JAPANESE("Japanese");
-
-        String name;
-
-        PlotType(String name){
-            this.name = name;
-        }
-
-        @Override
-        public String toString(){
-            return name;
-        }
-    }
-
-    private PlotType plotType;
+    private boolean isDrawingAverages;
 
     private final Font dateFont = new Font("Arial", Font.ITALIC, 10);
-    private final Font closingFont = new Font("Arial", Font.BOLD, 15);
-    private final Font otherDataFont = new Font("Arial", Font.PLAIN, 15);
+    private final Font valueFont = new Font("Arial", Font.BOLD, 12);
+    private final Font textFont = new Font("Arial", Font.PLAIN, 12);
 
-    private final Color backgroundColor = new Color(197, 202, 233);
-    private final Color simplePlotColor = new Color(48, 63, 159);
-    private final Color underSimplePlotColor = new Color(63, 81, 181, 64);
+    private final Color backgroundColor = Color.WHITE;
+    private final Color averagesColor = new Color(48, 63, 159);
 
-    private final Color japaneseIncreaseColor = Color.GREEN;
-    private final Color japaneseDecreaseColor = Color.RED;
+    private final Color volumeColor = new Color(66, 165, 255, 64);
 
-    private final Color detailsBackgroundColor = new Color(255, 255, 255, 127);
+    private final Color candleIncreaseColor = new Color(67, 160, 71);
+    private final Color candleDecreaseColor = new Color(198, 40, 40);
 
+    private final Color detailsBackgroundColor = new Color(255, 255, 255, 192);
+
+    /**
+     * Initializes default values of plot panel.
+     */
     public BitBayPlotPanel(){
         super();
         isMouseOver = false;
 
-        plotType = PlotType.AVERAGES;
+        isDrawingAverages = false;
 
         super.addMouseListener(this);
         super.addMouseMotionListener(this);
     }
 
+    /**
+     * Sets data used to plotting.
+     * @param data data array used to plotting.
+     */
     public void setData(List<BitBayCurrencyData> data) {
         this.data = data;
-        valueMin = data.stream().min(Comparator.comparing(BitBayCurrencyData::getAverage)).get().getMinimum() * 0.95;
-        valueMax = data.stream().max(Comparator.comparing(BitBayCurrencyData::getAverage)).get().getMaximum() * 1.05;
+        valueMin = data.stream().min(Comparator.comparing(BitBayCurrencyData::getMinimum)).get().getMinimum() * 0.95;
+        valueMax = data.stream().max(Comparator.comparing(BitBayCurrencyData::getMaximum)).get().getMaximum() * 1.05;
         valueRangeSize = valueMax - valueMin;
+
+        volumeMin = data.stream().min(Comparator.comparing(BitBayCurrencyData::getVolume)).get().getVolume();
+        volumeMax = data.stream().max(Comparator.comparing(BitBayCurrencyData::getVolume)).get().getVolume() * 1.25;
+        volumeRangeSize = volumeMax - volumeMin;
     }
 
-    public void setType(PlotType type){
-        plotType = type;
+
+    /**
+     * Sets boolean of average values should be drawn
+     * @param drawing boolean of average values should be drawn
+     */
+    public void setDrawingAverages(boolean drawing){
+        isDrawingAverages = drawing;
         repaint();
     }
 
+    /**
+     * Paints whole plot
+     * @param g Graphics on which plot is drawn.
+     */
     @Override
     public void paintComponent(Graphics g){
         super.paintComponent(g);
@@ -95,72 +102,50 @@ public class BitBayPlotPanel extends JPanel implements MouseListener, MouseMotio
         if(data != null && data.size() > 0){
             //width of one fragment of data cell
             final double dx = (double)getWidth() / data.size();
-            //y scale to transform currency value to height on the screen
-            final double yscale = getHeight() / valueRangeSize;
+            //value y scale to transform currency value to height on the screen
+            final double valueyscale = getHeight() / valueRangeSize;
+            //volume y scale to transform currency value to heigh on the screen
+            final double volumeyscale = getHeight() / volumeRangeSize;
 
-            drawPlot(g2d, dx, yscale);
+            g2d.setColor(backgroundColor);
+            g2d.fillRect(0, 0, getWidth(), getHeight());
+
+            drawVolume(g2d, dx, volumeyscale);
+            drawCandles(g2d, dx, valueyscale);
+            if(isDrawingAverages)
+                drawAverages(g2d, dx, valueyscale);
 
             if(isMouseOver){
-                drawDetailedCurrencyDataInfo(g2d, dx, yscale);
+                drawDetailedCurrencyDataInfo(g2d, dx);
             }
         }
     }
 
-    private void drawPlot(Graphics2D g2d, final double dx, final double yscale){
-        switch(plotType){
-            case CLOSINGS:
-                drawClosingsOrAverages(g2d, dx, yscale, true);
-                break;
-            case AVERAGES:
-                drawClosingsOrAverages(g2d, dx, yscale, false);
-                break;
-            case JAPANESE:
-                drawJapanese(g2d, dx, yscale);
-                break;
-        }
-    }
-
-    private void drawClosingsOrAverages(Graphics2D g2d, final double dx, final double yscale, final boolean closings){
+    private void drawAverages(Graphics2D g2d, final double dx, final double yscale){
         g2d.setStroke(new BasicStroke(1.5f));
-
-        g2d.setColor(backgroundColor);
-        g2d.fillRect(0, 0, getWidth(), getHeight());
 
         int i = 0;
         BitBayCurrencyData currencyData = data.get(i);
 
         int lastx = (int) ((i + 0.5) * dx);
-        double value = closings ? currencyData.getClosing() : currencyData.getAverage();
+        double value = currencyData.getAverage();
         int lasty = (int) ((valueMax - value) * yscale);
-
-        //draw minmax
-        g2d.setColor(simplePlotColor);
-        g2d.drawLine(lastx, (int) ((valueMax - currencyData.getMinimum()) * yscale), lastx, (int) ((valueMax - currencyData.getMaximum()) * yscale));
 
         for (i = 1; i < data.size(); ++i) {
             currencyData = data.get(i);
             int x = (int) ((i + 0.5) * dx);
-            value = closings ? currencyData.getClosing() : currencyData.getAverage();
+            value = currencyData.getAverage();
             int y = (int) ((valueMax - value) * yscale);
             //draw closing
-            g2d.setColor(simplePlotColor);
-            g2d.drawLine(lastx, lasty, x, y);
-            //draw under closing
-            g2d.setColor(underSimplePlotColor);
-            int[] xPoints = {lastx, x, x, lastx};
-            int[] yPoints = {lasty, y, getHeight(), getHeight()};
-            int nPoints = 4;
-            g2d.fillPolygon(xPoints, yPoints, nPoints);
-            //draw minmax
-            g2d.setColor(simplePlotColor);
-            g2d.drawLine(x, (int) ((valueMax - currencyData.getMinimum()) * yscale), x, (int) ((valueMax - currencyData.getMaximum()) * yscale));
+            g2d.setColor(averagesColor);
+            g2d.drawLine(lastx - 1, lasty, x - 1, y);
 
             lastx = x;
             lasty = y;
         }
     }
 
-    private void drawJapanese(Graphics2D g2d, final double dx, final double yscale){
+    private void drawCandles(Graphics2D g2d, final double dx, final double yscale){
         g2d.setStroke(new BasicStroke(1.5f));
 
         BitBayCurrencyData currencyData;
@@ -177,49 +162,64 @@ public class BitBayPlotPanel extends JPanel implements MouseListener, MouseMotio
             final int y = increase ? closingY : openingY;
             final int height = Math.max(Math.abs(closingY - openingY), 1);
 
-            Color candleColor = increase ? japaneseIncreaseColor : japaneseDecreaseColor;
+            Color candleColor = increase ? candleIncreaseColor : candleDecreaseColor;
 
             g2d.setColor(candleColor);
             g2d.fillRect(x + width / 5, y, width * 3 / 5, height);
 
             //draw minmax
             g2d.setColor(candleColor);
-            g2d.drawLine(x + width / 2, (int) ((valueMax - currencyData.getMinimum()) * yscale), x + width / 2, (int) ((valueMax - currencyData.getMaximum()) * yscale));
+            g2d.drawLine(x + width / 2 - 1, (int) ((valueMax - currencyData.getMinimum()) * yscale), x + width / 2 - 1, (int) ((valueMax - currencyData.getMaximum()) * yscale));
         }
     }
 
-    private void drawDetailedCurrencyDataInfo(Graphics2D g2d, double dx, double yscale) {
-        final int width = 200;
-        final int height = 100;
+    private void drawVolume(Graphics2D g2d, double dx, double yscale) {
+        g2d.setStroke(new BasicStroke(1.5f));
+
+        BitBayCurrencyData currencyData;
+        for (int i = 0; i < data.size(); ++i) {
+            currencyData = data.get(i);
+
+            int x = (int)(i * dx);
+            int width = (int)dx;
+            final int y = (int)((volumeMax - currencyData.getVolume()) * yscale);
+            final int height = (int)(currencyData.getVolume() * yscale);
+
+            g2d.setColor(volumeColor);
+            x += width / 10;
+            width = width * 4 / 5;
+            if(width <= 0) width = 1;
+            g2d.fillRect(x, y, width, height);
+        }
+    }
+
+    private void drawDetailedCurrencyDataInfo(Graphics2D g2d, double dx) {
+        final int width = 160;
+        final int height = 120;
 
         int coveredDataIndex = mouseX * data.size() / getWidth();
         if(coveredDataIndex >= data.size())
-            coveredDataIndex = data.size();
+            coveredDataIndex = data.size() - 1;
         if(coveredDataIndex < 0)
             coveredDataIndex = 0;
 
         BitBayCurrencyData currencyData = data.get(coveredDataIndex);
 
-        final int currencyX = (int)((coveredDataIndex + 0.5) * dx);
-
-        switch(plotType){
-            case AVERAGES: {
-                final int currencyAverageY = (int) ((valueMax - currencyData.getAverage()) * yscale);
-                g2d.setColor(simplePlotColor);
-                g2d.fillOval(currencyX - 3, currencyAverageY - 3, 7, 7);
-                break;
-            }
-            case CLOSINGS:{
-                final int currencyClosingY = (int)((valueMax - currencyData.getClosing()) * yscale);
-                g2d.setColor(simplePlotColor);
-                g2d.fillOval(currencyX - 3, currencyClosingY - 3, 7, 7);
-                break;
-            }
-        }
+        final int currencyX = (int)((coveredDataIndex + 0.5) * dx) - 1;
 
         g2d.setColor(Color.GRAY);
         g2d.drawLine(currencyX, 0, currencyX, getHeight());
 
+        g2d.drawLine(0, mouseY, getWidth(), mouseY);
+        double yvalueAtMousePos = valueRangeSize * (getHeight() - mouseY) / getHeight() + valueMin;
+        double yvolumeAtMousePos = volumeRangeSize * (getHeight() - mouseY) / getHeight() + volumeMin;
+
+        float horizontalLineFontSize = (getWidth() > getHeight() ? getHeight() / 25.0f : getWidth() / 25.0f);
+        g2d.setFont(textFont.deriveFont(horizontalLineFontSize));
+        g2d.drawString(String.format("%.2f", yvalueAtMousePos), getWidth() - horizontalLineFontSize * 3, mouseY);
+        g2d.drawString(String.format("%.2f", yvolumeAtMousePos), 10, mouseY);
+
+        //dialog drawing
         int dialogX = mouseX - width * 11 / 10;
         int dialogY = mouseY - height / 2;
 
@@ -231,6 +231,8 @@ public class BitBayPlotPanel extends JPanel implements MouseListener, MouseMotio
         g2d.setColor(detailsBackgroundColor);
         g2d.setStroke(new BasicStroke(1.0f));
         g2d.fillRoundRect(dialogX, dialogY, width, height, 10, 10);
+        g2d.setColor(Color.BLACK);
+        g2d.drawRoundRect(dialogX, dialogY, width, height, 10, 10);
 
         String dayOfWeek = currencyData.getPeriodStart().toLocalDate().getDayOfWeek().name();
         dayOfWeek = dayOfWeek.substring(0, 1).toUpperCase() + dayOfWeek.substring(1).toLowerCase();
@@ -243,15 +245,24 @@ public class BitBayPlotPanel extends JPanel implements MouseListener, MouseMotio
                 dayOfWeek,
                 localDate,
                 localTime),
-                dialogX + 10,
+                dialogX + 5,
                 dialogY + 15);
 
-        g2d.setFont(closingFont);
-        g2d.drawString(String.format("Closing: %.2f", currencyData.getClosing()), dialogX + 10, dialogY + 32);
-        g2d.setFont(otherDataFont);
+        g2d.setFont(textFont);
+        g2d.drawString("Opening", dialogX + width / 12, dialogY + height / 4);
+        g2d.drawString("Closing", dialogX + width * 7 / 12, dialogY + height / 4);
+        g2d.drawString("Minimum", dialogX + width / 12, dialogY + height * 2 / 4);
+        g2d.drawString("Maximum", dialogX + width * 7 / 12, dialogY + height * 2 / 4);
+        g2d.drawString("Volume", dialogX + width * 4 / 12, dialogY + height * 3 / 4);
+        g2d.setFont(valueFont);
+        g2d.drawString(String.format("%.2f", currencyData.getOpening()), dialogX + width / 12, dialogY + height * 3 / 8);
+        g2d.drawString(String.format("%.2f", currencyData.getClosing()), dialogX + width * 7 / 12, dialogY + height * 3 / 8);
+        g2d.drawString(String.format("%.2f", currencyData.getMinimum()), dialogX + width / 12, dialogY + height * 5 / 8);
+        g2d.drawString(String.format("%.2f", currencyData.getMaximum()), dialogX + width * 7 / 12, dialogY + height * 5 / 8);
+        g2d.drawString(String.format("%.2f", currencyData.getVolume()), dialogX + width * 4 / 12, dialogY + height * 7 / 8);
+        /*g2d.drawString(String.format("Closing: %.2f", currencyData.getClosing()), dialogX + 10, dialogY + 32);
         g2d.drawString(String.format("Average: %.2f", currencyData.getAverage()), dialogX + 10, dialogY + 52);
-        g2d.drawString(String.format("Opening: %.2f", currencyData.getOpening()), dialogX + 10, dialogY + 72);
-        g2d.drawString(String.format("Volume: %.2f", currencyData.getVolume()), dialogX + 10, dialogY + 92);
+        g2d.drawString(String.format("Volume: %.2f", currencyData.getVolume()), dialogX + 10, dialogY + 92);*/
     }
 
     private void setMousePosition(MouseEvent e){
