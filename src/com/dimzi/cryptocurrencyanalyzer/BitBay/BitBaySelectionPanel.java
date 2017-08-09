fc9ac623ebea65e73ffc9f0631b0489880bcfd8e
gorylpe <piotr.przestrzelski1@gmail.com>
2017-08-09 06:22:22 +0200

    drawing selection

--- src/com/dimzi/cryptocurrencyanalyzer/BitBay/BitBaySelectionPanel.java
+++ src/com/dimzi/cryptocurrencyanalyzer/BitBay/BitBaySelectionPanel.java
@@ -16,6 +16,9 @@ public class BitBaySelectionPanel extends JPanel {
     private double valueMin;
     private double valueMax;
 
+    private int rangeStart;
+    private int rangeEnd;
+
     private final Color averagesColor = new Color(48, 63, 159);
 
     /**
@@ -28,6 +31,11 @@ public class BitBaySelectionPanel extends JPanel {
         valueMin = data.stream().min(Comparator.comparing(BitBayCurrencyData::getMinimum)).get().getMinimum() * 0.95;
         valueMax = data.stream().max(Comparator.comparing(BitBayCurrencyData::getMaximum)).get().getMaximum() * 1.05;
         valueRangeSize = valueMax - valueMin;
+    }
+
+    public void setRange(int start, int end){
+        this.rangeStart = start;
+        this.rangeEnd = end;
     }
 
     /**
@@ -52,6 +60,36 @@ public class BitBaySelectionPanel extends JPanel {
         g2d.setColor(Color.WHITE);
         g2d.fillRect(0, 0, getWidth(), getHeight());
 
+        if(data != null && data.size() > 0) {
+            //width of one fragment of data cell
+            final double dx = (double) getWidth() / data.size();
+            //value y scale to transform currency value to height on the screen
+            final double yscale = getHeight() / valueRangeSize;
 
+            g2d.setStroke(new BasicStroke(1.5f));
+
+            int i = 0;
+            BitBayCurrencyData currencyData = data.get(i);
+
+            int lastx = (int) ((i + 0.5) * dx);
+            double value = currencyData.getAverage();
+            int lasty = (int) ((valueMax - value) * yscale);
+
+            for (i = 1; i < data.size(); ++i) {
+                currencyData = data.get(i);
+                int x = (int) ((i + 0.5) * dx);
+                value = currencyData.getAverage();
+                int y = (int) ((valueMax - value) * yscale);
+                //draw closing
+                g2d.setColor(averagesColor);
+                g2d.drawLine(lastx - 1, lasty, x - 1, y);
+
+                lastx = x;
+                lasty = y;
+            }
+
+            g2d.setColor(new Color(0, 255, 0, 64));
+            g2d.fillRect((int)(rangeStart * dx), 0, (int)((rangeEnd - rangeStart) * dx), getHeight());
+        }
     }
 }
