7e9f4bf781b7bb20700b10d07e3e1651e75daa78
gorylpe <piotr.przestrzelski1@gmail.com>
2017-08-09 05:42:25 +0200

    .

--- src/com/dimzi/cryptocurrencyanalyzer/Main.java
+++ src/com/dimzi/cryptocurrencyanalyzer/Main.java
@@ -7,14 +7,13 @@ public class Main {
 
     public static void main(String[] args) {
 
-        //com.dimzi.cryptocurrencyanalyzer.DatabaseManager.startAutoUpdateThread(10000);
-
         try {
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
 
             EventQueue.invokeLater(new Runnable() {
                 @Override
                 public void run() {
+
                     MainFrame mainFrame = new MainFrame();
                 }
             });
