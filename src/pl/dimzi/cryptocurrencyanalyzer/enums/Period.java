4b365f82d5aea51ad19772419c1f7011489f7714
Piotr Przestrzelski <p.przestrzelski@protonmail.com>
2017-11-03 01:55:39 +0100

    floor func fix

--- src/pl/dimzi/cryptocurrencyanalyzer/enums/Period.java
+++ src/pl/dimzi/cryptocurrencyanalyzer/enums/Period.java
@@ -4,7 +4,7 @@ import java.util.function.Function;
 
 public enum Period {
     DAILY(  (period) -> period + 24 * 3600,
-            (period) -> (period / (24 * 3600)) / 24 * 3600);
+            (period) -> (period / (24 * 3600)) * 24 * 3600);
 
     private String name;
     private Function<Long, Long> plusFunction;
