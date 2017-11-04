5d84044a250926225ee42966d689fc2b01aa29ab
Piotr Przestrzelski <p.przestrzelski@protonmail.com>
2017-11-04 11:24:39 +0100

    trade block model

---
+++ src/pl/dimzi/cryptocurrencyanalyzer/bitbay/model/TradeBlock.java
@@ -0,0 +1,27 @@
+package pl.dimzi.cryptocurrencyanalyzer.bitbay.model;
+
+public class TradeBlock {
+    Long dateStart;
+    Long dateEnd;
+
+    public TradeBlock(long dateStart, long dateEnd){
+        this.dateEnd = dateEnd;
+        this.dateStart = dateStart;
+    }
+
+    public long getDateStart(){
+        return dateStart;
+    }
+
+    public void setDateStart(long dateStart){
+        this.dateStart = dateStart;
+    }
+
+    public long getDateEnd(){
+        return dateEnd;
+    }
+
+    public void setDateEnd(long dateEnd){
+        this.dateEnd = dateEnd;
+    }
+}
